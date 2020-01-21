08b1514859cf00af6a3c58e1ff06e06faf2312bb
Kentaro Yoshioka <flatoutta@gmail.com>
2020-01-21 07:04:43 +0000

    These are assignment for php laravel lesson 21. Best regards.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -28,13 +28,42 @@
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar=collapse" id="navbarSupportedContent">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
               
             </ul>
+
+            <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
+  
+            {{-- 以下を追記 --}}
+            <!-- Authentication Links -->
+            {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
+            @guest
+                <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+            {{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+                {{-- 以上までを追記 --}}
             </ul>
-          </div>
+            </div>
         </div>
       </nav>
       
